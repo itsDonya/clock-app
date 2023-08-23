970534829593a6f162b5cfa756089ca2eab6a5a0
Donya <thisis2nya@gmail.com>
2023-08-23 12:26:08 +0330

    [UI] fix default layout display problem

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="w-screen h-screen bg-secondary">
+  <div class="w-screen h-screen bg-secondary flex items-start">
     <aside
       class="w-56 h-full bg-light rounded-tr-xl rounded-br-xl shadow-2xl shadow-primary"
     >
       <app-sidebar></app-sidebar>
     </aside>
-    <main>
+    <main class="w-full h-full">
       <nuxt />
     </main>
   </div>
